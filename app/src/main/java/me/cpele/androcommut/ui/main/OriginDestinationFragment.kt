faefdb52f323b28d208dd5ac4a6544b01a1d13f2
Christobal <geekarist@users.noreply.github.com>
2020-11-01 15:32:26 +0100

    Initial Android app

---
+++ app/src/main/java/me/cpele/androcommut/ui/main/OriginDestinationFragment.kt
@@ -0,0 +1,30 @@
+package me.cpele.androcommut.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.cpele.androcommut.R
+
+class OriginDestinationFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = OriginDestinationFragment()
+    }
+
+    private lateinit var viewModel: OriginDestinationViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.origin_destination_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OriginDestinationViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/me/cpele/androcommut/ui/main/OriginDestinationViewModel.kt
@@ -0,0 +1,7 @@
+package me.cpele.androcommut.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class OriginDestinationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
