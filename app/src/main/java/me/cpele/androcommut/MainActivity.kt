edb18c27d9dff432682afa437e0a545b08c19dfc
Christobal <geekarist@users.noreply.github.com>
2020-11-02 14:24:34 +0100

    Add NavHostFragment

--- app/src/main/java/me/cpele/androcommut/MainActivity.kt
+++ app/src/main/java/me/cpele/androcommut/MainActivity.kt
@@ -1,18 +1,12 @@
 package me.cpele.androcommut
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import me.cpele.androcommut.ui.od.OriginDestinationFragment
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, OriginDestinationFragment.newInstance())
-                    .commitNow()
-        }
     }
 }
