ba98b0ee7b4e732fbe10983ecdcdde03d9b8079e
Christobal <geekarist@users.noreply.github.com>
2020-11-07 16:20:53 +0100

    fixup! Create MVI Model interface

--- app/src/main/java/me/cpele/androcommut/Model.kt
+++ app/src/main/java/me/cpele/androcommut/Model.kt
@@ -3,7 +3,7 @@ package me.cpele.androcommut
 import androidx.lifecycle.LiveData
 
 interface Model<IntentionT, StateT, EffectT> {
+    fun dispatch(intention: IntentionT)
     val stateLive: LiveData<StateT>
     val effectLive: LiveData<Event<EffectT>>
-    fun dispatch(intention: IntentionT)
 }
