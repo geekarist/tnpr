48972041a4df706f2b29f824ff31161c3bd185ac
Christobal <geekarist@users.noreply.github.com>
2020-11-07 16:02:44 +0100

    Send autosuggestion query to ViewModel

--- app/src/main/java/me/cpele/androcommut/autosuggest/AutosuggestFragment.kt
+++ app/src/main/java/me/cpele/androcommut/autosuggest/AutosuggestFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.FlowPreview
 import me.cpele.androcommut.R
 
+@FlowPreview
 class AutosuggestFragment : Fragment() {
 
     companion object {
@@ -17,16 +21,18 @@ class AutosuggestFragment : Fragment() {
     private lateinit var viewModel: AutosuggestViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.autosuggest_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(AutosuggestViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
+        view.findViewById<EditText>(R.id.autosuggest_search_edit).addTextChangedListener {
+            viewModel.dispatch(AutosuggestViewModel.Intention.QueryEdited(it))
+        }
+    }
 }
--- app/src/main/java/me/cpele/androcommut/autosuggest/AutosuggestViewModel.kt
+++ app/src/main/java/me/cpele/androcommut/autosuggest/AutosuggestViewModel.kt
@@ -1,7 +1,34 @@
 package me.cpele.androcommut.autosuggest
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
+@FlowPreview
 class AutosuggestViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val queryFlow = MutableStateFlow<String?>(null)
+
+    private val debouncedQueryFlow = queryFlow.debounce(500)
+
+    init {
+        debouncedQueryFlow.onEach { query ->
+            Log.d(javaClass.simpleName, "To do: search for $query")
+        }.launchIn(viewModelScope)
+    }
+
+    fun dispatch(intention: Intention) {
+        when (intention) {
+            is Intention.QueryEdited -> queryFlow.value = intention.text.toString()
+        }
+    }
+
+    sealed class Intention {
+        data class QueryEdited(val text: CharSequence?) : Intention()
+    }
 }
