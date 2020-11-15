11a627b5dcb854d9d38093dd6203e85558d7e744
Christobal <geekarist@users.noreply.github.com>
2020-11-15 14:42:32 +0100

    Remove dependency to navigation

--- app/src/main/java/me/cpele/androcommut/MainActivity.kt
+++ app/src/main/java/me/cpele/androcommut/MainActivity.kt
@@ -2,11 +2,25 @@ package me.cpele.androcommut
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import me.cpele.androcommut.origdest.OriginDestinationFragment
+import me.cpele.androcommut.origdest.OriginDestinationFragmentDirections
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OriginDestinationFragment.Listener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
     }
+
+    override fun openAutosuggestOrigin(fragment: Fragment) =
+        fragment.findNavController().navigate(
+            OriginDestinationFragmentDirections.actionOriginDestinationFragmentToAutosuggestFragment()
+        )
+
+    override fun openAutosuggestDestination(fragment: Fragment) =
+        fragment.findNavController().navigate(
+            OriginDestinationFragmentDirections.actionOriginDestinationFragmentToAutosuggestFragment()
+        )
 }
--- app/src/main/java/me/cpele/androcommut/origdest/OriginDestinationFragment.kt
+++ app/src/main/java/me/cpele/androcommut/origdest/OriginDestinationFragment.kt
@@ -9,8 +9,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import me.cpele.androcommut.R
+import me.cpele.androcommut.origdest.OriginDestinationViewModel.Effect
 
 class OriginDestinationFragment : Fragment() {
 
@@ -23,6 +23,8 @@ class OriginDestinationFragment : Fragment() {
     private lateinit var originButton: Button
     private lateinit var destinationButton: Button
 
+    private var listener: Listener? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,7 +32,13 @@ class OriginDestinationFragment : Fragment() {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
+
         viewModel = ViewModelProvider(this).get(OriginDestinationViewModel::class.java)
+
+        listener = context as? Listener
+            ?: throw IllegalStateException(
+                "${context.javaClass.simpleName} has to implement ${Listener::class.simpleName}"
+            )
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -58,13 +66,12 @@ class OriginDestinationFragment : Fragment() {
 
         viewModel.effectLive.observe(viewLifecycleOwner) { event ->
             event.consume { effect ->
-                val navTarget = when (effect) {
-                    is OriginDestinationViewModel.Effect.NavigateToAutosuggest.Origin ->
-                        R.id.action_originDestinationFragment_to_autosuggestFragment
-                    is OriginDestinationViewModel.Effect.NavigateToAutosuggest.Destination ->
-                        R.id.action_originDestinationFragment_to_autosuggestFragment
+                when (effect) {
+                    is Effect.NavigateToAutosuggest.Origin ->
+                        listener?.openAutosuggestOrigin(this)
+                    is Effect.NavigateToAutosuggest.Destination ->
+                        listener?.openAutosuggestDestination(this)
                 }
-                findNavController().navigate(navTarget)
             }
         }
 
@@ -72,8 +79,18 @@ class OriginDestinationFragment : Fragment() {
             viewModel.dispatch(OriginDestinationViewModel.Intention.OriginClicked)
         }
 
-        view.findViewById<View>(R.id.od_destination_button).setOnClickListener {
+        destinationButton.setOnClickListener {
             viewModel.dispatch(OriginDestinationViewModel.Intention.DestinationClicked)
         }
+    }
+
+    override fun onDetach() {
+        listener = null
+        super.onDetach()
+    }
+
+    interface Listener {
+        fun openAutosuggestOrigin(fragment: Fragment)
+        fun openAutosuggestDestination(fragment: Fragment)
     }
 }
