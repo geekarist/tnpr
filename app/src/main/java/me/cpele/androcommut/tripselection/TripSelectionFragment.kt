98e9a87909b78842d4cc57b30e2415afb3b4d95e
Christobal <geekarist@users.noreply.github.com>
2020-11-30 08:31:34 +0100

    Display toast with origin and destination

--- app/src/main/java/me/cpele/androcommut/tripselection/TripSelectionFragment.kt
+++ app/src/main/java/me/cpele/androcommut/tripselection/TripSelectionFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import me.cpele.androcommut.R
@@ -21,6 +22,21 @@ class TripSelectionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.trip_selection_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val (origin, destination) = arguments
+            ?.let { TripSelectionFragmentArgs.fromBundle(it) }
+            ?.apply { origin to destination }
+            ?: throw IllegalArgumentException("Arguments must not be null")
+
+        Toast.makeText(
+            context,
+            "Origin: $origin, destination: $destination",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
