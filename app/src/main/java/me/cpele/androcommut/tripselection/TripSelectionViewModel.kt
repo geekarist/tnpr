2c4085ec988d4b67fa530cab6c9c72fc7761637a
Christobal <geekarist@users.noreply.github.com>
2020-12-08 08:54:06 +0100

    Call `NavitiaService.journeys()` (WIP)

--- app/src/main/java/me/cpele/androcommut/tripselection/TripSelectionViewModel.kt
+++ app/src/main/java/me/cpele/androcommut/tripselection/TripSelectionViewModel.kt
@@ -2,31 +2,58 @@ package me.cpele.androcommut.tripselection
 
 import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import me.cpele.afk.Component
 import me.cpele.afk.Event
 import me.cpele.afk.exhaust
+import me.cpele.androcommut.BuildConfig
+import me.cpele.androcommut.NavitiaJourneysResult
+import me.cpele.androcommut.NavitiaService
 import me.cpele.androcommut.tripselection.TripSelectionViewModel.*
 
-class TripSelectionViewModel :
-    ViewModel(),
-    Component<Intention, State, Effect> {
+class TripSelectionViewModel(
+    private val navitiaService: NavitiaService
+) : ViewModel(), Component<Intention, State, Effect> {
 
-    override fun dispatch(intention: Intention) {
-        when (intention) { // TODO: Don't over engineer: `dispatch()`, `Intention`, `State` and `Effect` are enough! Pure functions or middlewares are too much.
-            is Intention.Load -> Log.d(
-                javaClass.simpleName,
-                "Origin is ${intention.originId}: ${intention.originLabel}, " +
-                        "destination is: ${intention.destinationId}: ${intention.destinationLabel}"
-            )
-        }.exhaust()
-    }
-
+    private val _stateLive = MutableLiveData(State())
     override val stateLive: LiveData<State>
-        get() = TODO("Not yet implemented")
+        get() = _stateLive
 
     override val effectLive: LiveData<Event<Effect>>
         get() = TODO("Not yet implemented")
+
+    override fun dispatch(intention: Intention) {
+        when (intention) {
+            is Intention.Load -> handle(intention)
+        }.exhaust()
+    }
+
+    private fun handle(intention: Intention.Load) = viewModelScope.launch {
+        Log.d(
+            javaClass.simpleName,
+            "Origin is ${intention.originId}: ${intention.originLabel}, " +
+                    "destination is: ${intention.destinationId}: ${intention.destinationLabel}"
+        )
+
+        val navitiaResponse = withContext(Dispatchers.IO) {
+            navitiaService.journeys(
+                BuildConfig.NAVITIA_API_KEY,
+                intention.originId,
+                intention.destinationId
+            )
+        }
+
+        val model = navitiaResponse.toUiModel()
+
+        withContext(Dispatchers.Main) {
+            _stateLive.value = model
+        }
+    }
 
     sealed class Intention {
         data class Load(
@@ -37,10 +64,18 @@ class TripSelectionViewModel :
         ) : Intention()
     }
 
-    class State {
-
-    }
+    data class State(
+        val tripUiModels: List<TripUiModel>? = null
+    )
 
     sealed class Effect {
     }
+
+    data class TripUiModel(
+        val xx: String = TODO()
+    )
+}
+
+private fun NavitiaJourneysResult.toUiModel(): State? {
+    TODO("Not yet implemented")
 }
