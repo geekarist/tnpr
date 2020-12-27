9a6e0ea0a1b42561faa225c03303b654d0d9e5db
Christobal <geekarist@users.noreply.github.com>
2020-12-27 14:06:25 +0100

    Add process, Effect, Coeffect

--- app/src/main/java/me/cpele/androcommut/roadmap/RoadmapViewModel.kt
+++ app/src/main/java/me/cpele/androcommut/roadmap/RoadmapViewModel.kt
@@ -1,7 +1,20 @@
 package me.cpele.androcommut.roadmap
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.Flow
 
 class RoadmapViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
+
+private sealed class Coeffect {
+    data class Db(val todo: Nothing = TODO()) : Coeffect()
+    data class Http(val todo: Nothing = TODO()) : Coeffect()
+}
+
+private sealed class Effect {
+    data class Db(val todo: Nothing = TODO()) : Effect()
+    data class Http(val todo: Nothing = TODO()) : Effect()
+}
+
+private fun process(coeffectFlow: Flow<Coeffect>): Flow<Effect> = TODO()
