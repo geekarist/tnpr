c55036efa84c17ec1ed466a43f3dbd8cf0f0e0e8
Christobal <geekarist@users.noreply.github.com>
2021-01-14 09:08:29 +0100

    Implement roadmap view

--- app/src/main/java/me/cpele/androcommut/roadmap/RoadmapFragment.kt
+++ app/src/main/java/me/cpele/androcommut/roadmap/RoadmapFragment.kt
@@ -7,17 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
+import me.cpele.afk.Outcome
 import me.cpele.afk.ViewModelFactory
 import me.cpele.androcommut.CustomApp
 import me.cpele.androcommut.R
+import me.cpele.androcommut.core.Leg
+import me.cpele.androcommut.core.Trip
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
 class RoadmapFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = RoadmapFragment()
-    }
 
     private val viewModel: RoadmapViewModel by viewModels {
         ViewModelFactory {
@@ -34,8 +34,13 @@ class RoadmapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val adapter = RoadmapAdapter()
+        val recyclerView = view.findViewById<RecyclerView>(R.id.roadmap_recycler)
+        recyclerView.adapter = adapter
         viewModel.state.observe(viewLifecycleOwner) { state ->
             Log.d(javaClass.simpleName, "State: $state")
+            val items = items(state)
+            adapter.submitList(items)
         }
     }
 
@@ -48,3 +53,18 @@ class RoadmapFragment : Fragment() {
         viewModel.load(args.tripId)
     }
 }
+
+private fun items(state: Output.State?): List<RoadmapAdapter.Item> =
+    when (val outcome = state?.tripOutcome) {
+        is Outcome.Success -> items(outcome.value)
+        is Outcome.Failure -> emptyList()
+        null -> emptyList()
+    }
+
+fun items(trip: Trip): List<RoadmapAdapter.Item> = trip.legs.map { leg -> item(leg) }
+
+fun item(leg: Leg): RoadmapAdapter.Item =
+    RoadmapAdapter.Item(
+        description = leg.toString(),
+        duration = leg.duration
+    )
