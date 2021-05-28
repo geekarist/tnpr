5e8f38e2572b435849c2273b38837925f930cd15
cpele <geekarist@users.noreply.github.com>
2021-05-28 09:28:44 +0200

    Add logging interceptor

--- app/src/main/java/me/cpele/androcommut/CustomApp.kt
+++ app/src/main/java/me/cpele/androcommut/CustomApp.kt
@@ -3,6 +3,8 @@ package me.cpele.androcommut
 import android.app.Application
 import android.util.LruCache
 import me.cpele.androcommut.core.Journey
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -18,7 +20,17 @@ class CustomApp : Application() {
         super.onCreate()
         instance = this
 
+        val httpClient = OkHttpClient.Builder().apply {
+            if (BuildConfig.DEBUG) {
+                val interceptor = HttpLoggingInterceptor()
+                val level = HttpLoggingInterceptor.Level.BODY
+                interceptor.level = level
+                addNetworkInterceptor(interceptor)
+            }
+        }.build()
+
         navitiaService = Retrofit.Builder()
+            .client(httpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl("https://api.navitia.io")
             .build()
